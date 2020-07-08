09665ddea1d1e0434c85f7417d4cb15a58beede4
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-08 13:39:23 +0200

    Connexion bdd + affichage des accents

---
+++ includes/identifiant.php
@@ -0,0 +1,12 @@
+<?php
+# Permet d'établir la connexion avec la base de données
+try
+{
+$bdd = new PDO('mysql:host=localhost;dbname=forum', 'root', '');
+$bdd->exec("SET CHARACTER SET utf8");
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
