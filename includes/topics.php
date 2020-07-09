1c26229208fd0dbb7b8c184f95d8c384de4d7f39
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-09 11:07:27 +0200

    Modification de topics

--- includes/topics.php
+++ includes/topics.php
@@ -1,5 +1,7 @@
-<?php 
-# Création de topics accessible pour l'admin et le modérateur
+<?php
+# Création/modification/suppression de topics accessible pour l'admin et le modérateur
+
+# Création de topics
 if (isset($_POST['creer_topic'])) {
   $id_topic = $_POST['id'];
   $nom_topic = $_POST['nom_topic'];
@@ -48,8 +50,62 @@ if (isset($_POST['creer_topic'])) {
                  <td><?=$d['id'] ?></td>
                  <td><?=$d['topic'] ?></td>
                  <td><?=$d['statut'] ?></td>
+                 <td><a href="moderation.php?modifier_topic=<?php echo $d['id'] ?>"> Modifier </a></td>
+       					 <td><a href="moderation.php?supprimer_topic=<?php echo $d['id'] ?>"> Supprimer </a></td>
                <?php
              }
          } ?>
      </tbody>
    </table>
+
+   <?php # Modification des topics
+   if (isset($_GET['modifier_topic']))
+   {
+       $pdoselect2 = $bdd->prepare('SELECT * FROM topics WHERE id= :id');
+       $pdoselect2 ->bindValue(':id', $_GET['modifier_topic'], PDO::PARAM_INT);
+       $executepdo2= $pdoselect2->execute();
+       $info2= $pdoselect2->fetch();
+
+       if (isset($_POST['modifier_topic']))
+       {
+           $topic= $_POST['topic'];
+           $statut= $_POST['statut'];
+
+           $req2 = $bdd->prepare('UPDATE topics
+             SET topic = :topic,
+             statut = :statut
+             WHERE id = :id');
+
+           $req2->execute(array(
+           'topic' => $topic,
+           'statut' => $statut,
+           ));
+           if ($req2)
+           {
+               echo 'Modification enregistrée';
+               header("location: moderation.php?topics");
+           }
+        }
+        ?>
+        <form name="modification_topic" action="" method="POST">
+          <table border="0" align="center" cellspacing="2" cellpadding="2">
+            <tr align="center">
+              <td> Nom du topic </td>
+              <td><input type="text" name="topic" value="<?php echo $info2['topic'] ?>"></td>
+            </tr>
+            <tr align="center">
+  						<td> Statut </td>
+  						<td>
+                <select name="statut" id="statut">
+                  <option value="public"> Public </option>
+                  <option value="private"> Privé </option>
+                </select>
+              </td>
+            </tr>
+            <tr align="center">
+              <td><input name="modifier_topic" type="submit" value="Modifier topic"></td>
+            </tr>
+          </table>
+        </form>
+      <?php
+    } ?>
