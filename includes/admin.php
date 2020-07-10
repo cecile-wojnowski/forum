0100205a0f70d8fa1a2db3ea91975068fa99f9c5
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-10 11:54:22 +0200

    Formulaire, requête de bannissement / débannir

--- includes/admin.php
+++ includes/admin.php
@@ -126,20 +126,53 @@
   }
 
   # Suppression d'un utilisateur:
-  if (isset($_GET['supprimer_user'])) {
-                    try {
-                        $id = $_GET["supprimer_user"];
-                        $req = $bdd->prepare("DELETE FROM utilisateurs WHERE id = $id");
-                        $req->execute();
-                        echo 'Utilisateur supprimé';
-                        $delai = 1;
-                        $url = 'moderation.php?deleted';
-                        header("Refresh: $delai;url=$url");
-                    } catch (PDOException $e) {
-                        echo "Erreur : " . $e->getMessage();
-                    }
-                }
-  ?>
+  if (isset($_GET['supprimer_user']))
+  {
+    try
+    {
+      $id = $_GET["supprimer_user"];
+      $req = $bdd->prepare("DELETE FROM utilisateurs WHERE id = $id");
+      $req->execute();
+      echo 'Utilisateur supprimé';
+      $delai = 1;
+      $url = 'moderation.php?deleted';
+      header("Refresh: $delai;url=$url");
+    } catch (PDOException $e)
+    {
+      echo "Erreur : " . $e->getMessage();
+    }
+  }
 
+  # Bannissement
+  ?>
+  <h2 class="h2_moderation"> Bannissement :</h2>
+  <p class="p_admin"> Quel membre voulez-vous bannir ?</p>;
+    <form method="post" name="form_bannissement" action="moderation.php?action=ban">
+      <label for="membre"> Inscrivez le pseudo : </label>
+      <input type="text" id="membre" name="membre">
+      <input type="submit"name="form_bannissement" value="Envoyer"><br />'
+    </form>
+  <?php
+  if(isset($_POST['form_bannissement']))
+  {
+    $query = $bdd->query('SELECT id, login
+        FROM utilisateurs WHERE id_droits = 0');
+        if ($query->rowCount() > 0)
+        {
+          while($data = $query->fetch()) # A adapter !
+          {
+            ?>
+            <td><?=$data['login'] ?></td>
+            <input type="checkbox" name="<?php$data['id']?>"/>
+            Débannir<br />
+            <?php
+          } ?>
+            <p><input type="submit" value="Débannir" /></p></form>';
+            <?php
+        }
+        else echo '<p>Aucun membre banni pour le moment :p</p>';
+        $query->CloseCursor();
+  }
+  ?>
 
 </html>
