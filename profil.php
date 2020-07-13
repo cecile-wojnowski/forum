493b4fb6c688ed8b4cb01035fe57c50b4969986f
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-13 22:01:45 +0200

    Correction requête de modification

--- profil.php
+++ profil.php
@@ -15,9 +15,12 @@ $data=$query->fetch();
 <p><i>Vous êtes ici</i> : <a href="./index.php">Index du forum</a> --> Modification du profil; </p>
 
 <h1>Modifier son profil</h1>
-<form name="modifier_profil" method="post" action="profil.php?action=modifier" enctype="multipart/form-data">
+<form name="modifier_profil" method="post" action="profil.php" enctype="multipart/form-data">
   <fieldset><legend>Identifiants</legend>
-    Pseudo : <strong><?php stripslashes(htmlspecialchars($data['login'])) ?></strong><br />
+    <label for="pseudo"> Pseudo : </label>
+    <input type ="text" name="pseudo" id="pseudo">
+      <strong><?php stripslashes(htmlspecialchars($data['login'])) ?></strong>
+    </input>
     <label for="password">Nouveau mot de Passe :</label>
     <input type="password" name="password" id="password" /><br />
     <label for="confirm">Confirmer le mot de passe :</label>
@@ -51,29 +54,70 @@ $data=$query->fetch();
   </fieldset>
 
   <div class="button">
-  <input type="submit" value="Modifier son profil"/></div>
+  <input type="submit" name="modifier_profil" value="Modifier son profil"/></div>
   <input type="hidden" id="sent" name="sent" value="1" />
 </form>
 
 <?php
 
  if(isset($_POST['modifier_profil'])){
+   $temps = time();
 
-  $temps = time();
+   $pseudo = $_POST['pseudo'];
+   $signature = $_POST['signature'];
+   $email = $_POST['email'];
+   $website = $_POST['website'];
+   $localisation = $_POST['localisation'];
+   $pass = md5($_POST['password']);
+   $confirm = md5($_POST['confirm']);
 
-  $signature = $_POST['signature'];
-  $email = $_POST['email'];
-  $website = $_POST['website'];
-  $localisation = $_POST['localisation'];
-  $pass = md5($_POST['password']);
-  $confirm = md5($_POST['confirm']);
+  $query=$db->prepare('UPDATE utilisateurs
+  SET login = :pseudo, password = :password, email=:email, website=:website,
+  signature=:signature, localisation=:localisation
+  WHERE id=:id');
+  $query->bindValue(':pseudo',$pseudo,PDO::PARAM_STR);
+  $query->bindValue(':password',$pass,PDO::PARAM_STR);
+  $query->bindValue(':email',$email,PDO::PARAM_STR);
+  $query->bindValue(':website',$website,PDO::PARAM_STR);
+  $query->bindValue(':signature',$signature,PDO::PARAM_STR);
+  $query->bindValue(':localisation',$localisation,PDO::PARAM_STR);
+  $query->bindValue(':id',$id,PDO::PARAM_INT);
+  $query->execute();
+
+  var_dump($pseudo);
+
+  $query->CloseCursor();
+
 
   if ($pass != $confirm || empty($confirm) || empty($pass))
   {
        $mdp_erreur = "Votre mot de passe et votre confirmation diffèrent ou sont vides";
        $i++;
-  }
+     }
 }
+
+/*else{
+    echo'<h1>Modification interrompue</h1>';
+    echo'<p>Une ou plusieurs erreurs se sont produites pendant la modification du profil</p>';
+    echo'<p>'.$i.' erreur(s)</p>';
+    echo'<p>'.$mdp_erreur.'</p>';
+    echo'<p>'.$email_erreur1.'</p>';
+    echo'<p>'.$email_erreur2.'</p>';
+    echo'<p>'.$signature_erreur.'</p>';
+    echo'<p>'.$avatar_erreur.'</p>';
+    echo'<p>'.$avatar_erreur1.'</p>';
+    echo'<p>'.$avatar_erreur2.'</p>';
+    echo'<p>'.$avatar_erreur3.'</p>';
+    echo'<p> Cliquez <a href="./profil.php?action=modifier">ici</a> pour recommencer</p>';
+}*/
+
+/*
+# Stockage des données de la table dans des variables
+$pass=$data["password"];
+$email=$data["email"];
+$signature=$data["signature"];
+$localisation=$data["localisation"];
+$website=$data["website"];
 
 $i = 0;
 $mdp_erreur = NULL;
@@ -85,15 +129,6 @@ $avatar_erreur = NULL;
 $avatar_erreur1 = NULL;
 $avatar_erreur2 = NULL;
 $avatar_erreur3 = NULL;
-
-# Stockage des données de la table dans des variables
-$pass=$data["password"];
-$email=$data["email"];
-$signature=$data["signature"];
-$localisation=$data["localisation"];
-$website=$data["website"];
-
-
     //Vérification de l'adresse email
     //Il faut que l'adresse email n'ait jamais été utilisée (sauf si elle n'a pas été modifiée)
 
@@ -131,7 +166,6 @@ $website=$data["website"];
         $i++;
     }
 
-
     //Vérification de l'avatar
 
     if (!empty($_FILES['avatar']['size']))
@@ -167,8 +201,8 @@ $website=$data["website"];
         $extension_upload = strtolower(substr(  strrchr($_FILES['avatar']['name'], '.')  ,1));
         if (!in_array($extension_upload,$extensions_valides) )
         {
-                $i++;
-                $avatar_erreur3 = "Extension de l'avatar incorrecte";
+          $i++;
+          $avatar_erreur3 = "Extension de l'avatar incorrecte";
         }
     }
 ?>
@@ -191,56 +225,18 @@ $website=$data["website"];
                 $query->execute();
                 $query->CloseCursor();
         }
-
-        //Une nouveauté ici : on peut choisis de supprimer l'avatar
         if (isset($_POST['delete']))
         {
                 $query=$db->prepare('UPDATE forum_membres
-		SET membre_avatar=0 WHERE membre_id = :id');
+		                SET membre_avatar=0 WHERE membre_id = :id');
                 $query->bindValue(':id',$id,PDO::PARAM_INT);
                 $query->execute();
                 $query->CloseCursor();
         }
-
-        echo'<h1>Modification terminée</h1>';
-        echo'<p>Votre profil a été modifié avec succès !</p>';
-        echo'<p>Cliquez <a href="./index.php">ici</a>
-        pour revenir à la page d accueil</p>';
-
-        //On modifie la table
-
-        $query=$db->prepare('UPDATE utilisateurs
-        SET  password = :mdp, email=:email, website=:website,
-        signature=:sign, localisation=:loc
-        WHERE id=:id');
-        $query->bindValue(':mdp',$pass,PDO::PARAM_INT);
-        $query->bindValue(':email',$email,PDO::PARAM_STR);
-        $query->bindValue(':website',$website,PDO::PARAM_STR);
-        $query->bindValue(':sign',$signature,PDO::PARAM_STR);
-        $query->bindValue(':loc',$localisation,PDO::PARAM_STR);
-        $query->bindValue(':id',$id,PDO::PARAM_INT);
-        $query->execute();
-        $query->CloseCursor();
-    }
-    else
-    {
-        echo'<h1>Modification interrompue</h1>';
-        echo'<p>Une ou plusieurs erreurs se sont produites pendant la modification du profil</p>';
-        echo'<p>'.$i.' erreur(s)</p>';
-        echo'<p>'.$mdp_erreur.'</p>';
-        echo'<p>'.$email_erreur1.'</p>';
-        echo'<p>'.$email_erreur2.'</p>';
-        echo'<p>'.$msn_erreur.'</p>';
-        echo'<p>'.$signature_erreur.'</p>';
-        echo'<p>'.$avatar_erreur.'</p>';
-        echo'<p>'.$avatar_erreur1.'</p>';
-        echo'<p>'.$avatar_erreur2.'</p>';
-        echo'<p>'.$avatar_erreur3.'</p>';
-        echo'<p> Cliquez <a href="./profil.php?action=modifier">ici</a> pour recommencer</p>';
-    }
-
-
-
+  }
+    echo'<p>Cliquez <a href="./index.php">ici</a>
+    pour revenir à la page d accueil</p>';
+    */
 ?>
 </div>
 </body>
