cfb4fafdf1166ec21fb5fdab4d870bf83c53b253
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-15 21:48:54 +0200

    Appel de la fonction erreur_profil + requête UPDATE de la page Profil

---
+++ includes/profil_update.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include("identifiant.php");
+include("functions.php");
+
+if(erreur_profil($db) == false) {
+  if(isset($_POST['modifier_profil'])){
+
+    $pseudo = $_POST['pseudo'];
+    $signature = $_POST['signature'];
+    $email = $_POST['email'];
+    $website = $_POST['website'];
+    $localisation = $_POST['localisation'];
+    $pass = md5($_POST['password']);
+    $confirm = md5($_POST['confirm']);
+
+    $id=$_SESSION['id'];
+
+
+   $query=$db->prepare('UPDATE utilisateurs
+   SET login = :pseudo, password = :password, email=:email, website=:website,
+   signature=:signature, localisation=:localisation
+   WHERE id=:id');
+   $query->bindValue(':pseudo',$pseudo,PDO::PARAM_STR);
+   $query->bindValue(':password',$pass,PDO::PARAM_STR);
+   $query->bindValue(':email',$email,PDO::PARAM_STR);
+   $query->bindValue(':website',$website,PDO::PARAM_STR);
+   $query->bindValue(':signature',$signature,PDO::PARAM_STR);
+   $query->bindValue(':localisation',$localisation,PDO::PARAM_STR);
+   $query->bindValue(':id',$id,PDO::PARAM_INT);
+   $query->execute();
+ }
+}
+
+header("Location:../profil.php");
+?>
