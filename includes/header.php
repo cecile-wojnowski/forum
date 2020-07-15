845feb53c8f59621704b50a2a2a299afcc3585d4
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-15 15:57:05 +0200

    session_destroy

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <?php
-    include("identifiant.php");
-    include("debut.php");
+  include("identifiant.php");
+  include("debut.php");
 ?>
 
 <!DOCTYPE html>
@@ -23,20 +23,23 @@
       $message = "";
 
       if (isset($_SESSION['login'])) {
-        if ($_SESSION['id_droits']== 1) {
+        if ($_SESSION['id_droits']== 2)
+        {
           ?> <ul> <a href="index.php"><center>Accueil</center></a>
           <li><a href="profil.php">  Votre profil <i> <?php $_SESSION['login'] ?></i></a></li>
           <li><a href="topics.php"> Topics  </a></li>
-          <li><a href="index.php?deconnexion">déconnexion</a></li></ul>
-          <?php } elseif ($_SESSION['id_droits']== 2) { ?>
-                <ul> <li><a href="index.php">Accueil</a></li>
-                  <li><a href="profil.php">  Votre profil    <i><?php $_SESSION['login']?></i></a></li>
-                  <li><a href="topics.php"> les topics  </a> </li>
-                  <li><a href="index.php?deconnexion"><img src="https://img.icons8.com/fluent/48/000000/shutdown.png"/></a></li></ul>
-                <?php  } elseif ($_SESSION['id_droits']== 3) { ?>
-              <ul> <li><a href="index.php"><center>Accueil</center></a></li>
+          <li><a href="index.php?deconnexion"> Déconnexion</a></li></ul>
+          <?php
+        } elseif ($_SESSION['id_droits']== 3)
+          { ?>
+            <ul> <li><a href="index.php"> Accueil </a></li>
+            <li><a href="profil.php">  Votre profil    <i><?php $_SESSION['login']?></i></a></li>
+            <li><a href="topics.php"> Les topics  </a> </li>
+            <li><a href="index.php?deconnexion"><img src="https://img.icons8.com/fluent/48/000000/shutdown.png"/></a></li></ul>
+            <?php  } elseif ($_SESSION['id_droits']== 4) { ?>
+            <ul> <li><a href="index.php"><center> Accueil</center></a></li>
             <li><a href="profil.php">  Votre profil    <i><?php $_SESSION['login'] ?></i></a></li>
-            <li><a href="admin.php"> espace modération </a></li>
+            <li><a href="admin.php"> Espace modération </a></li>
             <li><a href="topics.php"> Topics  </a></li>
 
             <a href="index.php?deconnexion">
@@ -46,10 +49,10 @@
           </ul>
         <?php  }
         } else { ?><ul>
-          <li><a href="index.php">accueil</a></li>
-          <li><a href="inscription.php">inscription</a></li>
-          <li><a href="connexion.php">connexion</a></li>
-          <li><a href="topics.php">Topics</a>
+          <li><a href="index.php"> Accueil</a></li>
+          <li><a href="inscription.php"> Inscription</a></li>
+          <li><a href="connexion.php"> Connexion</a></li>
+          <li><a href="topics.php"> Topics</a>
           </li>
           <li style="float:right"><a class="active" href="recherche.php">faire une recherche</li></a>
           </ul>
@@ -57,11 +60,11 @@
 
 
         <?php  if (isset($_GET['deconnexion'])) {
-    unset($_SESSION['login']);
-    //au bout de 2 secondes redirection vers la page d'accueil
-    header("Refresh: 1; url=index.php");
+          session_destroy();
+          //au bout de 2 secondes redirection vers la page d'accueil
+          header("Refresh: 1; url=index.php");
 
-    echo "<p>Vous avez été déconnecté</p><br><p>Redirection vers la page d'accueil...</p>";
+          echo "<p>Vous avez été déconnecté</p><br><p>Redirection vers la page d'accueil...</p>";
 }
 
         $message = "";
