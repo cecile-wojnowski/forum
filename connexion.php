13e5fd4b183266dbfc719ab8e9895af23408989d
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-15 15:56:03 +0200

    corrections

--- connexion.php
+++ connexion.php
@@ -1,11 +1,9 @@
 <?php
+  session_start();
   $titre="Connexion";
-  include("includes/identifiant.php");
   include("includes/header.php");
-  include("includes/functions.php");
   echo '<p><i>Vous êtes ici</i> : <a href="./index.php">Index du forum</a> --> Connexion';
   echo '<h1>Connexion</h1>';
-
 ?>
 
 <!DOCTYPE html>
@@ -18,54 +16,54 @@
   </head>
   <body>
 
-
-
 <?php
   if (isset($_SESSION['id'])) {
-  echo "vous êtes déjà connecté, <a href='deconnexion.php' me déconnecter </a> ou <a href='profil.php'> voir mon profil </a> ";
+  echo "Vous êtes déjà connecté, <a href='deconnexion.php' me déconnecter </a> ou <a href='profil.php'> voir mon profil </a> ";
   } else{
-  if (!isset($_POST['login'])) { //On est dans la page de formulaire
-?>
-<form method="post" action="connexion.php">
-
-  <div class="container">
-        <h2 >Bon retour parmi nous !</h2>
-        <h4 >Connectez-vous avec votre login et mot de passe</h4>
-      <div class="form">
-<label for="">Login</label>        <input type="text" class="form-field animation a3" placeholder="login" name="login" id="pseudo">
-      <label for="">Password</label>  <input type="password" class="form-field animation a4" placeholder="Password" name="password" id="password">
-        <p class="animation a5">	<a href="./inscription.php">Pas encore inscrit ?</a></p>
-        <div class="button"> <button type="submit" value="Connexion">LOGIN</button></div>
+  if (!isset($_POST['login']))
+  { //On est dans la page de formulaire
+    ?>
+    <form method="POST" action="connexion.php">
+      <div class="container">
+        <h2>Connectez-vous avec votre login et mot de passe</h2>
+        <div class="form">
+          <label for="">Login</label>
+          <input type="text" class="form-field animation a3" placeholder="login" name="login" id="pseudo">
+          <label for="">Password</label>  <input type="password" class="form-field animation a4" placeholder="Password" name="password" id="password">
+          <p class="animation a5">	<a href="./inscription.php">Pas encore inscrit ?</a></p>
+          <div class="button">
+            <button type="submit" value="Connexion"> Se connecter </button>
+          </div>
+        </div>
       </div>
-    </div>
-	</body>
-	</html>';
 
-<?php } else {
-    $message='';
-    if (empty($_POST['login']) || empty($_POST['password'])) { //Oublie d'un champ
+    <?php
+    }else{
+      $message='';
+      if (empty($_POST['login']) || empty($_POST['password'])) { //Oublie d'un champ
         $message = '<p>une erreur s\'est produite pendant votre identification.
-	Vous devez remplir tous les champs</p>
-	<p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
-    } else { //On check le mot de passe
+      	Vous devez remplir tous les champs</p>
+      	<p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
+      } else { //On check le mot de passe
         $query=$db->prepare('SELECT password, id, id_droits, login
         FROM utilisateurs WHERE login = :login');
         $query->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
         $query->execute();
 
         $data=$query->fetch();
-        if ($data['password'] == md5($_POST['password'])) { // Acces OK !
-            $_SESSION['login'] = $data['login'];
-            $_SESSION['id_droits'] = $data['id_droits'];
-            $_SESSION['id'] = $data['id'];
-            $message = '<p>Bienvenue '.$data['login'].'votre id_droits est'.$data['id_droits'].',
-			vous êtes maintenant connecté!</p>
-			<p>Cliquez <a href="./index.php">ici</a>
-			pour revenir à la page d accueil</p> et ici pour <a href="profil.php"> voir votre profil </a';
+        if ($data['password'] == md5($_POST['password']))
+        { // Acces OK !
+          $_SESSION['login'] = $data['login'];
+          $_SESSION['id_droits'] = $data['id_droits'];
+          $_SESSION['id'] = $data['id'];
+          $message = '<p>Bienvenue '.$data['login'].'votre id_droits est'.$data['id_droits'].',
+    			vous êtes maintenant connecté!</p>
+    			<p>Cliquez <a href="./index.php">ici</a>
+    			pour revenir à la page d accueil</p> et ici pour <a href="profil.php"> voir votre profil </a>';
         } else { // Acces pas OK !
             $message = '<p>Une erreur s\'est produite
 	    pendant votre identification.<br /> Le mot de passe ou le pseudo
-            entré n\'est pas correcte.</p>';
+            entré n\'est pas correct.</p>';
         }
         $query->CloseCursor();
     }
@@ -73,9 +71,7 @@
 }
 }
 
-?>
-
-<?php include("includes/footer.php");
+ include("includes/footer.php");
  ?>
 </body>
 </html>
