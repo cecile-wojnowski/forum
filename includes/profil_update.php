b60cc83e4f1a3e42f6b1cc1f86cc5a6bbb54f01d
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-16 21:23:39 +0200

    Corrections requêtes comprenant l'avatar

--- includes/profil_update.php
+++ includes/profil_update.php
@@ -14,12 +14,14 @@ if(erreur_profil($db) == false) {
     $pass = md5($_POST['password']);
     $confirm = md5($_POST['confirm']);
 
+    $nomavatar = $_POST['avatar'];
+
     $id=$_SESSION['id'];
 
 
    $query=$db->prepare('UPDATE utilisateurs
    SET login = :pseudo, password = :password, email=:email, website=:website,
-   signature=:signature, localisation=:localisation
+   signature=:signature, localisation=:localisation, avatar =:avatar
    WHERE id=:id');
    $query->bindValue(':pseudo',$pseudo,PDO::PARAM_STR);
    $query->bindValue(':password',$pass,PDO::PARAM_STR);
@@ -27,14 +29,16 @@ if(erreur_profil($db) == false) {
    $query->bindValue(':website',$website,PDO::PARAM_STR);
    $query->bindValue(':signature',$signature,PDO::PARAM_STR);
    $query->bindValue(':localisation',$localisation,PDO::PARAM_STR);
+   $query->bindValue(':avatar',$nomavatar,PDO::PARAM_STR);
    $query->bindValue(':id',$id,PDO::PARAM_INT);
    $query->execute();
  }
  if(!empty($_FILES['avatar']['size']))
  {
+   # Transporte l'avatar et retourne son nom
     $nomavatar=move_avatar($_FILES['avatar']);
-    $query=$db->prepare('UPDATE forum_membres SET membre_avatar = :avatar
-      WHERE membre_id = :id');
+    $query=$db->prepare('UPDATE utilisateurs SET avatar = :avatar
+      WHERE id = :id');
     $query->bindValue(':avatar',$nomavatar,PDO::PARAM_STR);
     $query->bindValue(':id',$id,PDO::PARAM_INT);
     $query->execute();
@@ -47,7 +51,7 @@ if(erreur_profil($db) == false) {
       $query->execute();
       $query->CloseCursor();
     }
+  }
 }
-
 header("Location:../profil.php");
 ?>
