f0d9a421f8f57add2ed5987665f653393f00340e
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-16 10:53:30 +0200

    Indentation et ajout balise <li> pour le lien Accueil

--- includes/header.php
+++ includes/header.php
@@ -25,35 +25,38 @@
       if (isset($_SESSION['login'])) {
         if ($_SESSION['id_droits']== 2)
         {
-          ?> <ul> <a href="index.php"><center>Accueil</center></a>
-          <li><a href="profil.php">  Votre profil <i> <?php $_SESSION['login'] ?></i></a></li>
-          <li><a href="topics.php"> Topics  </a></li>
-          <li><a href="index.php?deconnexion"> Déconnexion</a></li></ul>
+          ?>
+          <ul>
+            <li><a href="index.php"><center>Accueil</center></a></li>
+            <li><a href="profil.php">  Votre profil <i> <?php $_SESSION['login'] ?></i></a></li>
+            <li><a href="topics.php"> Topics  </a></li>
+            <li><a href="index.php?deconnexion"> Déconnexion</a></li>
+          </ul>
           <?php
         } elseif ($_SESSION['id_droits']== 3)
           { ?>
-            <ul> <li><a href="index.php"> Accueil </a></li>
-            <li><a href="profil.php">  Votre profil    <i><?php $_SESSION['login']?></i></a></li>
-            <li><a href="topics.php"> Les topics  </a> </li>
-            <li><a href="index.php?deconnexion"><img src="https://img.icons8.com/fluent/48/000000/shutdown.png"/></a></li></ul>
-            <?php  } elseif ($_SESSION['id_droits']== 4) { ?>
-            <ul> <li><a href="index.php"><center> Accueil</center></a></li>
-            <li><a href="profil.php">  Votre profil    <i><?php $_SESSION['login'] ?></i></a></li>
-            <li><a href="moderation.php"> Espace modération </a></li>
-            <li><a href="topics.php"> Topics  </a></li>
-
-            <a href="index.php?deconnexion">
-            <img src="https://img.icons8.com/fluent/48/000000/shutdown.png"/></a>
-            <li style="float:right"><a class="active" href="#about">Faire une recherche</a></li>
-
-          </ul>
+            <ul>
+              <li><a href="index.php"> Accueil </a></li>
+              <li><a href="profil.php">  Votre profil    <i><?php $_SESSION['login']?></i></a></li>
+              <li><a href="topics.php"> Les topics  </a> </li>
+              <li><a href="index.php?deconnexion"><img src="https://img.icons8.com/fluent/48/000000/shutdown.png"/></a></li>
+            </ul>
+              <?php  } elseif ($_SESSION['id_droits']== 4) { ?>
+            <ul>
+              <li><a href="index.php"><center> Accueil</center></a></li>
+              <li><a href="profil.php">  Votre profil    <i><?php $_SESSION['login'] ?></i></a></li>
+              <li><a href="moderation.php"> Espace modération </a></li>
+              <li><a href="topics.php"> Topics  </a></li>
+                  <a href="index.php?deconnexion"><img src="https://img.icons8.com/fluent/48/000000/shutdown.png"/></a>
+              <li style="float:right"><a class="active" href="#about">Faire une recherche</a></li>
+            </ul>
         <?php  }
-        } else { ?><ul>
-          <li><a href="index.php"> Accueil</a></li>
-          <li><a href="inscription.php"> Inscription</a></li>
-          <li><a href="connexion.php"> Connexion</a></li>
-          <li><a href="topics.php"> Topics</a>
-          </li>
-          <li style="float:right"><a class="active" href="recherche.php">faire une recherche</li></a>
+        } else { ?>
+          <ul>
+            <li><a href="index.php"> Accueil</a></li>
+            <li><a href="inscription.php"> Inscription</a></li>
+            <li><a href="connexion.php"> Connexion</a></li>
+            <li><a href="topics.php"> Topics</a></li>
+            <li style="float:right"><a class="active" href="recherche.php">faire une recherche</li></a>
           </ul>
-<?php  }   ?>
+          <?php  }   ?>
