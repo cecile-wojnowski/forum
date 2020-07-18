5e4ced744c0f246b67caa3b5595e01f78140bbc5
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-18 12:42:41 +0200

    Rajout des liens dans le footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,10 @@
+<div class="navbar_bottom">
+      <a href="index.php"><center>Accueil</center></a>
+      <a href="topics.php"> Topics  </a>
+      <a href="regles.php"> Accéder aux CGU du forum </a>
+      <a href="contact.php">Nous contacter</a>
+
+    </div>
 <?php
 echo'<div id="footer">
 <h2>
@@ -34,4 +41,3 @@ $query->CloseCursor();
 </div>
 </body>
 </html>
-
