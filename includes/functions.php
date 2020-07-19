2ac06c005f3f1a237a55965dd27d4145266156b7
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-19 10:23:34 +0200

    Correction chemin

--- includes/functions.php
+++ includes/functions.php
@@ -12,7 +12,7 @@ function move_avatar($avatar)
     $extension_upload = strtolower(substr(  strrchr($avatar['name'], '.')  ,1));
     $name = time();
     $nomavatar = str_replace(' ','',$name).".".$extension_upload;
-    $name = "../images/avatars/".str_replace(' ','',$name).".".$extension_upload;
+    $name = "images/avatars/".str_replace(' ','',$name).".".$extension_upload;
     move_uploaded_file($avatar['tmp_name'],$name);
     return $nomavatar;
 }
