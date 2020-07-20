531e27b66742e437be6cad173a43734dbcb9a091
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-20 11:44:10 +0200

    Correction chemin avatar

--- profil.php
+++ profil.php
@@ -64,7 +64,7 @@ $website=$data["website"];
     <label><input type="checkbox" name="delete" value="Delete" />
     Supprimer l avatar</label>
     Avatar actuel :
-    <img src="images/avatars/<?php echo $data['avatar']; ?>"
+    <img src="img/avatars/<?php echo $data['avatar']; ?>"
   alt="pas d avatar" height = "100px" width = "100px"/> <br />
     <label for="signature">Signature :</label>
     <textarea cols="40" rows="4" name="signature" id="signature"> <?php stripslashes(htmlspecialchars($data['signature'])) ?></textarea>
