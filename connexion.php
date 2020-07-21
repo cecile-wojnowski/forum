572d1ade81d14d6b57a340c4f2cf00718eef43b7
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-21 14:59:28 +0200

    Condition empêchant la connexion si user banni (id_droits = 5)

--- connexion.php
+++ connexion.php
@@ -16,11 +16,12 @@
   <body>
 
 <?php
+
   if (isset($_SESSION['id'])) {
   echo "Vous êtes déjà connecté, <a href='deconnexion.php' me déconnecter </a> ou <a href='profil.php'> voir mon profil </a> ";
   } else{
     if (!isset($_POST['login']))
-    { //On est dans la page de formulaire
+    {
     ?>
     <form method="POST" action="connexion.php">
       <div class="container">
@@ -45,26 +46,35 @@
         $message = '<p>une erreur s\'est produite pendant votre identification.
       	Vous devez remplir tous les champs. </p>
       	<p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
-      } else { # Si les champs sont remplis
+      }
+      else
+      { # On cherche s'il y a un user ayant un login identique à celui entré dans le formulaire
         $query=$db->prepare('SELECT password, id, id_droits, login FROM utilisateurs WHERE login = :login');
         $query->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
         $query->execute();
 
-          if ($query->rowCount() == 1 )
+          if ($query->rowCount() == 1 ) # S'il y a au moins un résultat, on va chercher les infos dans la table
          {
-          $mdp = $_POST['password']; # Hashe le mdp entré par l'user
+          $mdp = $_POST['password'];
           $data=$query->fetch();
-          $mdpbdd = $data['password']; # Stocke le mdp hashé de la bdd
-            if(password_verify($mdp, $mdpbdd)) { # Ne passe pas par cette condition
-
+          $mdpbdd = $data['password'];
+            # Si le mot de passe entré correspond à celui en bdd, on permet la connexion
+            if(password_verify($mdp, $mdpbdd))
+            {
+              if($data['id_droits'] == 5){
+                echo "Vous avez été banni, vous ne pouvez donc pas vous connecter.";
+              }else{
+              # On stocke dans des variables de session les données récupérées dans la table
               $_SESSION['login'] = $data['login'];
               $_SESSION['id_droits'] = $data['id_droits'];
               $_SESSION['id'] = $data['id'];
+
               $message = '<p>Bienvenue '.$data['login'].'votre id_droits est '.$data['id_droits'].',
                     vous êtes maintenant connecté!</p>
                     <p>Cliquez <a href="./index.php">ici</a>
                     pour revenir à la page d accueil</p> et ici pour <a href="profil.php"> voir votre profil </a>';
-          } else { // Acces pas OK !
+            }
+          }else {
             $message = '<p>Une erreur s\'est produite pendant votre identification.<br /> Le mot de passe ou le pseudo
             entré n\'est pas correct.</p>';
           }
