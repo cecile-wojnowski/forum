734971eb8f6426c4420f77f905914211c9e391a2
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-21 19:36:21 +0200

    Filtrage des topics en fonction de l'id_droits et du statut privé/public

--- topics.php
+++ topics.php
@@ -1,6 +1,5 @@
 
 <?php
-
 include("includes/identifiant.php");
 include("includes/header.php");
 include("includes/bbcode.php");
@@ -18,27 +17,41 @@ include("includes/bbcode.php");
 <div class="topics">
 
 <?php
-$req = $db->query('SELECT * FROM topics ');
-
-foreach($req->fetchAll() as $post):
-
-   ?>
-
-
-
-<div class="card">
-  <img src="img/forum1.webp" alt="Avatar" style="width:100%">
-  <div class="container">
-    <h4><?= $post['topic']; ?></h4>
-<p> <a href="conversations.php?id=<?= $post['id']; ?>">Voir les conversations</a></p>
+# Si une session est ouverte et que l'id_droit est celui des modérateurs ou des admins...
+# On affiche tous les topics, qu'ils soient publics ou privés
+if(isset($_SESSION['id_droits'])){
+  if($_SESSION['id_droits'] == 3 OR 4)
+  {
+    $req = $db->query('SELECT * FROM topics');
+    foreach($req->fetchAll() as $post):
+    ?>
+    <div class="card">
+      <img src="img/forum1.webp" alt="Avatar" style="width:100%">
+      <div class="container">
+        <h4><?= $post['topic']; ?></h4>
+        <a href="conversations.php?id=<?= $post['id']; ?>">Voir les conversations</a>
+      </div>
+    </div>
+    <?php endforeach;
+  }
+}else # Sinon, pour tous les autres utilisateurs, on affiche que les topics publics
+{
+  $req = $db->query('SELECT * FROM topics WHERE statut = "public"');
+  foreach($req->fetchAll() as $post):
+  ?>
+  <div class="card">
+    <img src="img/forum1.webp" alt="Avatar" style="width:100%">
+    <div class="container">
+      <h4><?= $post['topic']; ?></h4>
+      <a href="conversations.php?id=<?= $post['id']; ?>">Voir les conversations</a>
+    </div>
   </div>
-</div>
-
-
-<?php endforeach;
+  <?php endforeach;
+}
 ?>
 
 </div>
+
 
     </main>
 
