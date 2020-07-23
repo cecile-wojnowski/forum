46fe723141af56a71fd4a247e9ec225d703f709e
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-23 20:42:55 +0200

    Création d'un fichier signaler + utilisation de SESSION pour les msg

---
+++ signaler.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include("includes/identifiant.php");
+if(isset($_SESSION["id"]))
+{
+  $id_message = $_GET['id_message'];
+  $id_conversation = $_GET['id_conversation'];
+
+  $stmt_signaler = $db->prepare("INSERT INTO signaler (id_message) VALUES (?)");
+  $stmt_signaler->execute([$id_message]);
+
+  $_SESSION["message"] = "Le message a été signalé aux administrateurs.";
+  header("Location:messages.php?id=$id_conversation");
+} else {
+  $_SESSION["message"] = "Vous devez être connecté pour signaler un message";
+  header("Location:topics.php");
+}
+?>
