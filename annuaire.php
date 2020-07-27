b0a4d3861e69a9398f0f3a35b9471e27157e2b08
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-27 16:50:29 +0200

    CSS annuaire

--- annuaire.php
+++ annuaire.php
@@ -5,43 +5,37 @@ include("includes/header.php");
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <table>
-      <h2> Tout les membres inscrits sur le forum </h2>
-<div class="annuaire">
+  <h2> Annuaire des membres du forum </h2>
+    <div class="annuaire">
 
     <?php $query = $db->prepare('SELECT *
     FROM utilisateurs ORDER BY login ASC');
-$query->execute();
-while ($data = $query->fetch())
-{
-?>
-<table class="" style="text-align:right">
-
+    $query->execute(); ?>
+  <table>
+    <thead>
+      <th> Profil </th>
+      <th> Localisation </th>
+      <th> Website </th>
+      <th> E-mail </th>
+    </thead>
+    <?php while ($data = $query->fetch())
+    {
+    ?>
     <tbody>
       <tr>
-        <td scope=""><a href="voirprofil.php?m=<?php echo $data['id']; ?>"><?php echo $data['login'] ?></a></td>
-        <th scope=""><?php echo $data['localisation'] ?></th>
-        <td scope=""><?php echo $data['website'] ?></td>
-        <td scope=""><?php echo $data['email'] ?></td>
+        <td><a class= "a_annuaire" href="voirprofil.php?m=<?php echo $data['id']; ?>"><?php echo $data['login'] ?></a></td>
+        <td><?php echo $data['localisation'] ?></td>
+        <td><?php echo $data['website'] ?></td>
+        <td><?php echo $data['email'] ?></td>
       </tr>
     </tbody>
-  </table>
-
-
-</table>
-
-
 
 <?php
 }
-?></div>
+?>
+</table>
+
+</div>
 <?php include ('includes/footer.php') ?>
 
   </body>
